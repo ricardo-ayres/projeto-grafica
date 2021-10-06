acb57c7b162ded9f4e3171060ba06eb5a7d41ec5
Ricardo B. Ayres <ricardo.bosqueiro.ayres@gmail.com>
2021-10-05 22:57:43 -0300

    criando uma view improvisada que calcula o orcamento

--- grafica/urls.py
+++ grafica/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('orcamentos/', include('orcamentos.urls')),
+    path('', include('orcamentos.urls')),
     path('admin/', admin.site.urls),
 ]
--- orcamentos/views.py
+++ orcamentos/views.py
@@ -8,3 +8,28 @@ def index(request):
     produtos = Produto.objects.order_by('nome')
     context = {'produtos': produtos}
     return render(request, 'orcamentos/index.html', context)
+
+def orcamento(request):
+    total = 0
+    pedido = []
+    for pid in request.POST:
+        try:
+            produto = Produto.objects.get(pk=pid)
+        except:
+            # skip iteration if not a product id
+            continue
+
+        quantidade = int(request.POST[pid])
+        subtotal = produto.preco * quantidade
+        item = {
+            'produto': produto.nome,
+            'preco': produto.preco,
+            'quantidade': quantidade,
+            'subtotal': subtotal
+        }
+        pedido.append(item)
+        total += subtotal
+
+    context = {'pedido': pedido, 'total': total}
+    return render(request, 'orcamentos/orcamento.html', context)
+    
