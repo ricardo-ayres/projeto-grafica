44253e15383e387e8a2e2cb8a52157cc4a12a141
Ricardo B. Ayres <ricardo.bosqueiro.ayres@gmail.com>
2021-11-24 23:01:31 +0000

    production mods

--- grafica/settings.py
+++ grafica/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-57@8cv+*k_&zu&4voa1iy3%smz8)9p(%@v1%!(m-plp*-0@dp2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['e00m.duckdns.org']
 
 
 # Application definition
@@ -118,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
