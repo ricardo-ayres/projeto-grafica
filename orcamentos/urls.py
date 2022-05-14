6339ea67a1febaaa5c2ec3c0fc1178778d5b36da
Ricardo B. Ayres <ricardo.bosqueiro.ayres@gmail.com>
2022-05-14 01:17:49 -0300

    Implementando um cliente besta em JS

--- orcamentos/urls.py
+++ orcamentos/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('catalogo/', views.catalogo, name="catalogo"),
     path('orcamento/', views.orcamento, name="orcamento"),
+    path('app/', views.app, name="app"),
 ]
